040ca00c00ef57a12ba547ca18127dd5df0246b2
Niels J. de Wit <ndewit@gmail.com>
2015-08-05 22:47:00 +0200

    *space*

--- demo-stub-ref/SetupDialog.h
+++ demo-stub-ref/SetupDialog.h
@@ -11,7 +11,7 @@ bool SetupDialog(
 	float &aspectRatio,         // Return value is either -1.f (correct automatically) or a forced output ratio.
 	unsigned int &multiSamples, // Return value is either 1 (switch it off, set Quality to 0) or 2/4/8 (minimum Direct3D 11.0 hardware requirement).
 	bool &windowed,
-	bool &vSync,          // Should be ignored in windowed mode (see DirectX documentation).
+	bool &vSync,                // Should be ignored in windowed mode (see DirectX documentation).
 	IDXGIFactory1 &DXGIFactory);
 
 #endif // PLAYER_DIALOG_H
